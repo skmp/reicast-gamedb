508bc1739befab7374cfd5be8a326ae107359188
djleven <info@e-leven.net>
2019-05-03 15:34:58 -0400

    init

---
+++ src/i18n/en-us/index.js
@@ -0,0 +1,45 @@
+export default {
+  app: {
+    name: 'Reicast db',
+    description: 'Reicast Game Compatibility Database',
+    version: '0.0.1',
+    links: 'Reicast Links'
+  },
+  appLinks: {
+    docs: {
+      label: 'User Guide',
+      sub: '',
+      url: 'https://reicast.com/guide/',
+      icon: 'fa fa-book'
+    },
+    github: {
+      label: 'Contribute',
+      sub: '',
+      url: 'https://github.com/reicast',
+      icon: 'fa fa-hands-helping'
+    },
+    forum: {
+      label: 'Forum',
+      url: 'https://dcemulation.org/phpBB/viewforum.php?f=86',
+      icon: 'fa fa-bullhorn'
+    },
+    chat: {
+      label: 'Chat',
+      sub: '',
+      url: 'http://chat.reicast.com/',
+      icon: 'fab fa-discord'
+    },
+    twitter: {
+      label: 'Twitter',
+      sub: '',
+      url: 'https://twitter.com/reicastdc',
+      icon: 'fab fa-twitter'
+    },
+    facebook: {
+      label: 'Facebook',
+      sub: '',
+      url: 'https://www.facebook.com/reicastdc',
+      icon: 'fab fa-facebook'
+    }
+  }
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Reicast" src="statics/reicast-logo.png">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
