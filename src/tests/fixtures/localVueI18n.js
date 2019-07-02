31f2196a725511cbf0320c563fb1e6d3e53a7efc
djleven <info@e-leven.net>
2019-07-02 01:38:10 -0400

    add Footer test and VueI18n fixture

---
+++ src/tests/fixtures/localVueI18n.js
@@ -0,0 +1,18 @@
+import { createLocalVue } from '@vue/test-utils';
+import VueI18n from 'vue-i18n'
+import messages from '../i18n'
+
+// Use a local vue
+const localVue = createLocalVue()
+localVue.use(VueI18n)
+
+// Create new i18n instance
+module.exports = {
+  localVue,
+  i18n: new VueI18n({
+    locale: 'en-us',
+    fallbackLocale: 'en-us',
+    silentTranslationWarn: true,
+    messages
+  })
+}
